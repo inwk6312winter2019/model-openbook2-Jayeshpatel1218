3bb3298642f4f9e90833956dba2b20358e75d18d
Jayeshpatel1218 <jy331399@dal.ca>
2019-02-06 21:06:57 +0000

    task1 performed

---
+++ openbook1task3.py
@@ -0,0 +1,14 @@
+t=[]
+def transit(filename):
+	f=open(filename)
+	for line in f:
+		f1=tuple()
+		line=line.split(",")
+		str_name=line[2]
+		full_name=line[4]
+		from_str=line[6]
+		to_str=line[7]
+		f1=(str_name,full_name,from_str,to_str)
+		t.append(f)
+	return t
+print(transit("Street_Centrelines.csv"))
