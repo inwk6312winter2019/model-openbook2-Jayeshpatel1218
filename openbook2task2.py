d370bea0de1a0be83a03fcc31a36cd686d4205fd
jayeshpatel <jayeshpatel1218>
2019-02-06 19:55:15 -0500

    commit

---
+++ openbook2task2.py
@@ -0,0 +1,17 @@
+import csv
+
+def acc_areterial(s1,s2):
+  street = []
+  restreet = []
+  s1 = open('street_centrelines.csv','r')
+  s2 = open('Bus_stops.csv','r')
+  for area in s1:
+   area = area.split(",")
+   if area[10] == 'arterial'
+   if stee not in street:
+    street.append(stee)
+  for str in street:
+  if stre not in street:
+   bus = []
+
+print(bus)
